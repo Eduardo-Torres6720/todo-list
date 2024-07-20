4992edfcfc323204ee4a5dd0b254a1d1042ee44a
Eduardo-Torres6720 <eduardotorres6720@gmail.com>
2024-07-20 16:20:23 -0300

    feat: add constructor and implement UserDetails

--- src/main/java/com/example/todo_list/domain/user/User.java
+++ src/main/java/com/example/todo_list/domain/user/User.java
@@ -1,5 +1,10 @@
 package com.example.todo_list.domain.user;
 
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,7 +21,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class User {
+public class User implements UserDetails{
 
     @Id @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
@@ -24,4 +29,21 @@ public class User {
     private String login;
 
     private String password;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAuthorities'");
+    }
+
+    @Override
+    public String getUsername() {
+        // TODO Auto-generated method stub
+        return login;
+    }
 }
