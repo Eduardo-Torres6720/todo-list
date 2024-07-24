6512da3adba8eb2680c364507a905dc3f71dd256
Eduardo-Torres6720 <eduardotorres6720@gmail.com>
2024-07-24 15:21:56 -0300

    feat: create task repository

---
+++ src/main/java/com/example/todo_list/repositories/TaskRepository.java
@@ -0,0 +1,11 @@
+package com.example.todo_list.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.todo_list.domain.task.Task;
+
+public interface TaskRepository extends JpaRepository<Task, String> {
+    List<Task> findByUserId(String userid);
+}
